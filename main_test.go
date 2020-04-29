96db108311329cdad917aea2288cd1322272746b
KJ <info.j.landwoner.k@gmail.com>
2020-04-29 11:49:45 +0900

    fix main_test.go

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"net"
 	"testing"
 
@@ -11,16 +12,17 @@ func TestCheckSync(t *testing.T) {
 	dnsName := "g.jlandowner.com"
 	addr, err := net.LookupHost(dnsName)
 	assert.Nil(t, err)
+	ctx := context.Background()
 
 	t.Run("OK", func(t *testing.T) {
 		ttl := 10
-		ok := checkSync(addr, dnsName, ttl)
-		assert.Equal(t, ok, true)
+		ok := checkSync(ctx, addr, dnsName, ttl)
+		assert.Equal(t, true, ok)
 	})
 	t.Run("NG", func(t *testing.T) {
 		expectedIP := []string{"1.1.1.1", "2,2,2,2"}
 		ttl := 10
-		ok := checkSync(expectedIP, dnsName, ttl)
-		assert.Equal(t, ok, true)
+		ok := checkSync(ctx, expectedIP, dnsName, ttl)
+		assert.Equal(t, false, ok)
 	})
 }
